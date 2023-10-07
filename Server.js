1f348775a34f3eef6c3e0bad88e1dbd549dea5be
anandmurti012 <anand.raj431@gmail.com>
2023-10-08 00:16:14 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/ToDoRoute');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then( () =>{
+    console.log('Db connected')
+}).catch((error) =>{
+    console.log(error);
+});
+
+app.use(routes);
+
+const PORT = process.env.PORT || 5010;
+
+
+app.listen(PORT, () =>
+    console.log(`Server is listening on ${PORT}`)
+);
